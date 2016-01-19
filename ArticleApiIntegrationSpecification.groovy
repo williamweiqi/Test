58a9c8b0979d2246b717b3349c0d76eb1924c955
Wei Qi <wei@parcelpoint.com.au>
2016-01-19 12:59:17 +1100

    no message

--- ArticleApiIntegrationSpecification.groovy
+++ ArticleApiIntegrationSpecification.groovy
@@ -1,1 +1,1 @@
-packageau.com.parcelpoint.api.integration.v4_1importau.com.parcelpoint.api.helper.ObjectBuilder
+packageau.com.parcelpoint.api.integration.v4_1importau.com.parcelpoint.api.helper.ObjectBuilder
